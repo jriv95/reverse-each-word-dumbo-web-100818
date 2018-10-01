414d5daf9088a4ad92c67511352ef79d3fed8b7b
Joel Rivera <joel.rivera7995@gmail.com>
2018-10-01 16:18:41 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,20 @@
+def reverse_each_word(string)
+  new_array = string.split(" ")
+  each_new_array = []
+  new_array.each do |item|
+    item.reverse
+    each_new_array << item.reverse
+    end
+    each_new_array.join(" ")
+end
+
+
+def reverse_each_word(string)
+  new_array = string.split(" ")
+  each_new_array = []
+  new_array.collect do |item|
+    item.reverse
+    each_new_array << item.reverse
+    end
+    each_new_array.join(" ")
+end
